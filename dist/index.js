0a0c482a8a12c3492078596c6eac2695f458ae4c
maadeha Shaikh <121812405+maadehashaikh@users.noreply.github.com>
2026-08-27 23:39:32 +0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(user) {
+    return `hello ${user}`;
+}
+console.log(greet("ali"));
+const user = "chai or TS";
+console.log(greet(user));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function greet(user: string): string {
+  return `hello ${user}`;
+}
+
+
+
+console.log(greet("ali"));
+
+
+const user: string = "chai or TS";
+
+console.log(greet(user));
