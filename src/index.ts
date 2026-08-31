a76d9057ad867a641810b66fcc9dff1344017618
maadeha Shaikh <121812405+maadehashaikh@users.noreply.github.com>
2026-08-31 23:44:15 +0500

    first commit

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,15 @@ console.log(greet(user));
 
 
 
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/maadehashaikh/tsChaicode.git
+// git push -u origin main
+
+
+
 
 
 
